9f6ce2540573af3f1ab41823faaf3ab0b855003f
mrom3r <73850076+mrom3r@users.noreply.github.com>
2022-11-12 00:34:29 +0100

    minimal progress yeah

--- src/game/gameLoop.cpp
+++ src/game/gameLoop.cpp
@@ -26,65 +26,21 @@ int main() {
     if (!vertexBufferInitialized) return -1;
 
     // Create and compile our GLSL program from the shaders
-    programID = LoadShaders(R"(..\src\game\shader\VertexShader.vertexshader)",
-                            R"(..\src\game\shader\FragmentShader.fragmentshader)");
+    program_ID = LoadShaders(R"(..\src\game\shader\VertexShader.vertexshader)",
+                             R"(..\src\game\shader\FragmentShader.fragmentshader)");
 
     //start animation loop until escape key is pressed
     do {
-        static glm::mat2 myR{1.0f, 0.0f,
-                             0.0f, 1.0f};
-
-        static glm::vec2 myT{0.0f, 0.0f};
-
-        static glm::vec2 triangleVertex1{-0.5f, -0.5f};
-        static glm::vec2 triangleVertex2{0.5f, -0.5f};
-        static glm::vec2 triangleVertex3{0.0f, 0.5f};
-
-        triangleVertex1 = myR * triangleVertex1 + myT;
-        triangleVertex2 = myR * triangleVertex2 + myT;
-        triangleVertex3 = myR * triangleVertex3 + myT;
-
-        updateAnimationLoop();
+        drawGame();
     } // Check if the ESC key was pressed or the window was closed
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
     //Cleanup and close window
     cleanupVertexBuffer();
-    glDeleteProgram(programID);
+    glDeleteProgram(program_ID);
     closeWindow();
 
     return 0;
-}
-
-void updateAnimationLoop() {
-    // Clear the screen
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Use our shader
-    glUseProgram(programID);
-
-    // 1rst attribute buffer : vertices
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glVertexAttribPointer(
-            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-            3,                  // size
-            GL_FLOAT,           // type
-            GL_FALSE,           // normalized?
-            0,                  // stride
-            (void *) nullptr    // array buffer offset
-    );
-
-    // Draw the triangle
-    glDrawArrays(GL_TRIANGLES, 0, vertexBuffer_size); // 3 indices starting at 0 -> 1 triangle
-
-    glDisableVertexAttribArray(0);
-
-    // Swap buffers
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-
-    initializeVertexBuffer();
 }
 
 bool initializeWindow() {
@@ -102,10 +58,10 @@ bool initializeWindow() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow(1024, 768, "Tutorial 02 - Red triangle", nullptr, nullptr);
+    window = glfwCreateWindow(1024, 768, "FutureShock", nullptr, nullptr);
     if (window == nullptr) {
-        fprintf(stderr,
-                "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        fprintf(stderr,"Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible."
+                       " Try the 2.1 version of the tutorials.\n");
         getchar();
         glfwTerminate();
         return false;
@@ -130,21 +86,47 @@ bool initializeWindow() {
 }
 
 bool initializeVertexBuffer() {
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    vertexBuffer_size = 3;
-
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    glGenVertexArrays(1, &vertex_array_ID);
+    glBindVertexArray(vertex_array_ID);
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
 
     return true;
 }
 
+void drawGame() {
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Use our shader
+    glUseProgram(program_ID);
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void *) nullptr    // array buffer offset
+    );
+
+    // Draw the triangle
+    glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+    glDisableVertexAttribArray(0);
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
 bool cleanupVertexBuffer() {
-    glDeleteBuffers(1, &vertexBuffer);
-    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteBuffers(1, &vertex_buffer);
+    glDeleteVertexArrays(1, &vertex_array_ID);
     return true;
 }
 
--- src/game/gameLoop.hpp
+++ src/game/gameLoop.hpp
@@ -6,25 +6,23 @@
 #include <glm/detail/type_mat.hpp>
 #include <glm/vec2.hpp>
 #include <glm/detail/type_mat2x2.hpp>
+#include <vector>
 
-//
-GLfloat g_vertex_buffer_data[]{
-        -0.5, 0.5, 0.0f,
-        0.5, -0.5, 0.0f,
-        0.0, 0.0, 0.0f,
+static const GLfloat vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
 };
 
-
 // some global variables for handling the vertex buffer
-GLuint vertexBuffer{};
-GLuint VertexArrayID{};
-GLint vertexBuffer_size{};
+GLuint vertex_buffer{};
+GLuint vertex_array_ID{};
 
 // program ID of the shaders, required for handling the shaders with OpenGL
-GLuint programID{};
+GLuint program_ID{};
 
 int main(); //<<< main function, called at startup
-void updateAnimationLoop(); //<<< updates the animation loop
+void drawGame(); //<<< updates the animation loop
 bool initializeWindow(); //<<< initializes the window using GLFW and GLEW
 bool initializeVertexBuffer(); //<<< initializes the vertex buffer array and binds it OpenGL
 bool cleanupVertexBuffer(); //<<< frees all recources from the vertex buffer

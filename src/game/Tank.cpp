9dc0142a133d27067453809ef773ce7220ba3e7e
mrom3r <73850076+mrom3r@users.noreply.github.com>
2022-11-14 09:22:34 +0100

    Tank updated

--- src/game/Tank.cpp
+++ src/game/Tank.cpp
@@ -2,9 +2,12 @@
 #include "Tank.hpp"
 
 Tank::Tank() {
-    // body
     GameObject player{Game::rectangle_positions({}, 0.1, 0.05)};
     game_objects.push_back(player);
-    GameObject player_canon{Game::rectangle_positions({}, 0.05, 0.008), {0.0f, 0.02}, -90};
-    game_objects.push_back(player_canon);
+
+    GameObject tower{Game::rectangle_positions({}, 0.05, 0.03), {0.0f, 0.04}};
+    game_objects.push_back(tower);
+
+    GameObject canon{Game::rectangle_positions({}, 0.12, 0.008), {0.0f, 0.06}, -90};
+    game_objects.push_back(canon);
 }
